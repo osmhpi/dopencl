80a4d9c5ea40ce78067bb814c9d3cba428b94d7a
Joan Bruguera <joanbrugueram@gmail.com>
2020-02-08 16:33:37 +0100

    Add LIBRARY_PATH for easily linking to dOpenCL libraries when building, like with the include headers.

--- dopenclenv.fish
+++ dopenclenv.fish
@@ -2,6 +2,7 @@
 
 if status --is-login
     set -gx PATH $PATH "/opt/dopencl/bin"
+    set -gx LIBRARY_PATH $LIBRARY_PATH "/opt/dopencl/lib"
     set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH "/opt/dopencl/lib"
     set -gx C_INCLUDE_PATH $C_INCLUDE_PATH "/opt/dopencl/include"
     set -gx CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH "/opt/dopencl/include"
