f76a28041c09d77547f0d50e9022f52546489151
Joan Bruguera <joanbrugueram@gmail.com>
2020-02-08 16:32:40 +0100

    Fish profile script: Since the script is sourced for both login and non-login shells, only add the paths on a login shell (i.e. like bash does with /etc/profile.d script).

--- dopenclenv.fish
+++ dopenclenv.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -gx PATH $PATH "/opt/dopencl/bin"
-set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH "/opt/dopencl/lib"
-set -gx C_INCLUDE_PATH $C_INCLUDE_PATH "/opt/dopencl/include"
-set -gx CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH "/opt/dopencl/include"
+if status --is-login
+    set -gx PATH $PATH "/opt/dopencl/bin"
+    set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH "/opt/dopencl/lib"
+    set -gx C_INCLUDE_PATH $C_INCLUDE_PATH "/opt/dopencl/include"
+    set -gx CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH "/opt/dopencl/include"
+end
