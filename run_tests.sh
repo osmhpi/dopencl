619ec19089238f07b13e812065963c1b1d5697c3
Joan Bruguera <joanbrugueram@gmail.com>
2020-02-17 20:12:47 +0100

    Test execution: Use optimized 842 implementation for faster execution.

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Build
 mkdir -p build_tests
 cd build_tests
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-ignored-attributes -DBUILD_UNIT_TESTS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Wno-ignored-attributes -DBUILD_UNIT_TESTS=ON -DUSE_OPTIMIZED_SERIAL_IMPLEMENTATION=ON ..
 make -j$(nproc)
 
 TEMPDIR="testdir" && rm -Rf "$TEMPDIR" && mkdir -p "$TEMPDIR"
