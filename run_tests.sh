707a3b4c705feaa76fa2e81d78af3356565a4029
Joan Bruguera <joanbrugueram@gmail.com>
2020-04-28 17:14:29 +0200

    run_tests.sh script: Add solid logic for detecting dcld startup, if netcat is installed.

--- run_tests.sh
+++ run_tests.sh
@@ -17,7 +17,15 @@ export DCL_LOG_LEVEL=VERBOSE
 trap 'pkill dcld; pkill -9 dcld;' EXIT
 (mkdir -p n1 && cd n1 && ../../daemon/dcld 127.0.0.1:30000) &
 (mkdir -p n2 && cd n2 && ../../daemon/dcld 127.0.0.1:30001) &
-sleep 1 # TODO: Find a better way to wait for dcld startup
+
+# Wait for the daemons to start up
+if command -v nc >/dev/null; then
+    while ! nc -z 127.0.0.1 30000; do sleep 0.1; done
+    while ! nc -z 127.0.0.1 30001; do sleep 0.1; done
+else
+    echo "WARNING: netcat not installed, can not probe for dcld startup. Execution may fail!"
+    sleep 1 # And pray for the best
+fi
 printf "127.0.0.1:30000\n127.0.0.1:30001\n" > dcl.nodes
 
 find ../test/ -maxdepth 1 -type f -executable -exec echo "**" {} "**" \; -exec env LD_PRELOAD=../icdpp/libdOpenCL.so {} -l unit_scope \;
