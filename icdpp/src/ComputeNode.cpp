osmhpi/dopencl

commit 7701f4f100278a80fe483694b70832fcaf678986

Remove hacky and test code.
This removes some code that was introduced in the first revisions after deriving this fork from the original SVN r1819 which looks like test code or hacky code used during testing. Possibly some this code was also used for initial integration with CloudCL, so it will be reintroduced if required if problems arise. The tests still run without this code.

icdpp/src/ComputeNode.cpp
@@ -63,7 +63,6 @@
 #include <cstddef>
 #include <memory>
 #include <vector>
-#include <iostream>
 
 _cl_compute_node_WWU::_cl_compute_node_WWU(
 		cl_platform_id platform,
@@ -96,7 +95,6 @@ void _cl_compute_node_WWU::destroy() {
 void _cl_compute_node_WWU::getDevices(
 		cl_device_type type,
 		std::vector<cl_device_id>& devices) const {
-    type = CL_DEVICE_TYPE_ALL;
     switch (type) {
     case CL_DEVICE_TYPE_DEFAULT:
     case CL_DEVICE_TYPE_CPU:

icdpp/src/Platform.cpp
@@ -207,7 +207,7 @@ void _cl_platform_id::destroyComputeNode(cl_compute_node_WWU computeNode) {
 		assert(numErased == 1);
 	}
 
-	//_communicationManager->destroyComputeNode(remote);
+	_communicationManager->destroyComputeNode(remote);
 
 	delete computeNode;
 }

icdpp/src/cl_wwu_dcl.cpp
@@ -73,7 +73,6 @@
 #include <iterator>
 #include <memory>
 #include <stdexcept>
-#include <iostream>
 
 /**
  * @brief Initialization routine.
@@ -208,7 +207,6 @@ cl_int clGetComputeNodeInfoWWU(
 cl_int clGetDeviceIDsFromComputeNodeWWU(cl_compute_node_WWU compute_node,
         cl_device_type device_type, cl_uint num_entries, cl_device_id *devices,
         cl_uint *num_devices) {
-    std::cout << "Retrieving Device IDs!" << std::endl;
     /* TODO Validate compute node */
     if (!compute_node) return CL_INVALID_NODE_WWU;
 

commit 558fe5478dbe2fcad4ed93b21a61d987ee9b4129

icdpp/src/ComputeNode.cpp: Fix clGetDeviceIDs with multiple bitflags.
This properly handles clGetDeviceIDs with multiple device types (e.g. CPU OR GPU), which is required for CloudCL / Aparapi to work properly (since it uses by default the bitflags CL_​DEVICE_​TYPE_​CPU | CL_​DEVICE_​TYPE_​GPU | CL_​DEVICE_​TYPE_​ACCELERATOR).

The wrong implementation was previously 'hacked up' by always interpreting the value as CL_DEVICE_TYPE_ALL. This hack was removed on 7701f4f100278a80fe483694b70832fcaf678986, which broke the CloudCL integration. Now this commit properly fixes the root issue so no hack is required.

icdpp/src/ComputeNode.cpp
@@ -95,37 +95,38 @@ void _cl_compute_node_WWU::destroy() {
 void _cl_compute_node_WWU::getDevices(
 		cl_device_type type,
 		std::vector<cl_device_id>& devices) const {
-    switch (type) {
-    case CL_DEVICE_TYPE_DEFAULT:
-    case CL_DEVICE_TYPE_CPU:
-    case CL_DEVICE_TYPE_GPU:
-    case CL_DEVICE_TYPE_ACCELERATOR:
+    static const cl_device_type ALL_DEVICE_TYPE_BITFLAGS =
+        CL_DEVICE_TYPE_DEFAULT
+        | CL_DEVICE_TYPE_CPU
+        | CL_DEVICE_TYPE_GPU
+        | CL_DEVICE_TYPE_ACCELERATOR
 #if defined(CL_VERSION_1_2)
-    case CL_DEVICE_TYPE_CUSTOM:
+        | CL_DEVICE_TYPE_CUSTOM
 #endif // #if defined(CL_VERSION_1_2)
-        /* device type is valid; return all devices of specified type */
-        devices.clear(); // clear output list
+    ;
 
-        for (const auto& device : _devices) {
-            cl_device_type deviceType;
-
-            device->getInfo(CL_DEVICE_TYPE, sizeof(cl_device_type), &deviceType, nullptr);
-            if ((deviceType & type)) {
-                devices.push_back(device.get());
-            }
-        }
-
-        break;
-    case CL_DEVICE_TYPE_ALL:
-	devices.clear(); // clear output list
-        
-	/* just return all devices */
-        for (const auto& device : _devices) {
-	    devices.push_back(device.get());
-        }
-        break;
-    default:
+    if (type != CL_DEVICE_TYPE_ALL && (type & ~ALL_DEVICE_TYPE_BITFLAGS) != 0)
         throw dclicd::Error(CL_INVALID_DEVICE_TYPE);
+
+    /* device type is valid; return all devices of specified type */
+    devices.clear(); // clear output list
+
+    for (const auto& device : _devices) {
+        cl_device_type deviceType;
+        device->getInfo(CL_DEVICE_TYPE, sizeof(cl_device_type), &deviceType, nullptr);
+
+        // "CL_DEVICE_TYPE_ALL: All OpenCL devices available in the system
+        //                      except CL_DEVICE_TYPE_CUSTOM devices."
+        if (type == CL_DEVICE_TYPE_ALL && deviceType == CL_DEVICE_TYPE_CUSTOM)
+            continue;
+
+        // If type is not CL_DEVICE_TYPE_ALL then check intersection of type bitmask
+        // TODO: CL_DEVICE_TYPE_DEFAULT probably needs special treatment here,
+        // but I think the default device is not yet handled at all by dOpenCL...
+        if (type != CL_DEVICE_TYPE_ALL && (deviceType & type) == 0)
+            continue;
+
+        devices.push_back(device.get());
     }
 
 	if (devices.empty()) {